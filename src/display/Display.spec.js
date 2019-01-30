e692a1f8d0efcb070a417fbf8c3b54b64e34fef4
Mr. Metelus <Mdmetelus@gmail.com>
2019-01-30 17:34:23 -0500

    Display tests implemented

--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -1,1 +1,38 @@
 // Test away!
+import 'jest-dom/extend-expect';
+import React from 'react';
+import { cleanup, render } from 'react-testing-library';
+import Display from './Display'
+
+afterEach(cleanup);
+
+describe("<Display/>", () => {
+    it('renders the correct display detail', () => {
+        const { debug, getByTestId } = await render(<Display />);
+        debug();
+        const locker = getByTestId("locker");
+        const closeable = getByTestId("closeable");
+
+        //getByClassName('lockedClass');
+        //getByClassName("closedClass");
+
+        expect(locker).toHaveTextContent(/unlocked/i);
+        expect(closeable).toHaveTextContent(/open/i);
+    });
+
+    it("displays closed if closed is a true statment", () => {
+        const { getByText } = render(<Display closed={true} />);
+
+        getByText(/closed/i);
+
+    });
+
+    it("shold display the locked if locked prop is true", () => {
+        const shouldBeLocked = render(<Display locked={true} />).getByText;
+
+        shouldBeLocked(/locked/i);
+
+    });
+
+
+});
