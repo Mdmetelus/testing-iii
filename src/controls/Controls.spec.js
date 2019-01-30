80de1ecb7f44536228940dced97c2092e64c8862
Mr. Metelus <Mdmetelus@gmail.com>
2019-01-30 17:35:48 -0500

    updates

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -3,3 +3,19 @@ import 'jest-dom/extend-expect';
 import React from 'react';
 import { cleanup, render } from 'react-testing-library';
 import Controls from "./Controls";
+
+descriobe("<Controls/>", () => {
+    describe('', () => {
+        it('', async () => {
+            const{ debug, } = await render(<Controls toggleLocked toggleClosed />);
+
+        })
+    })
+
+    describe('', () => {
+        it('', async () => {
+            const  = render(<Controls locked={true} closed={true} />);
+
+        })
+    })
+});
--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,1 +1,6 @@
 // Test away
+import 'jest-dom/extend-expect';
+import React from 'react';
+import { cleanup, render } from 'react-testing-library';
+import 'jest-dom/extend-expect';
+import Dashboard from './Dashboard';
