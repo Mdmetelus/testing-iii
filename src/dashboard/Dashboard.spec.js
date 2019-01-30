a5b4a579e6c42d4ad5799d79857ec9a78942d004
Mr. Metelus <Mdmetelus@gmail.com>
2019-01-30 17:46:09 -0500

    teststhat dashboard and controls are shown

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -4,3 +4,16 @@ import React from 'react';
 import { cleanup, render } from 'react-testing-library';
 import 'jest-dom/extend-expect';
 import Dashboard from './Dashboard';
+
+
+describe('That it should show both Display and Controls', () => {
+    it('should show the display component', ()=> {
+        const {container} = render(<Dashboard />)
+        expect(container.firstChild).toHaveClass('display')
+    }) 
+    it('should show the controls component next', ()=> {
+        const {container} = render(<Dashboard />)
+
+        expect(container.firstChild.nextSibling).toHaveClass('controls')
+    })
+}) 
